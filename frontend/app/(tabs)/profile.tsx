3ea6db1f55b5b0506c989c32d1ee5fe76543d9e7
csabaistrab <csabaistrab2003@gmail.com>
2025-10-28 12:43:08 +0100

    További fájlok icializálása

---
+++ frontend/app/(tabs)/profile.tsx
@@ -0,0 +1,3 @@
+export default function ProfileScreen() {
+  return null; // Üresen hagyjuk most
+}
---
+++ frontend/app/create_account.tsx
@@ -0,0 +1,3 @@
+export default function CreateAccountScreen() {
+  return null; // Üresen hagyjuk most
+}
