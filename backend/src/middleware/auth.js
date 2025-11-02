281f3d54e23fe9e469b69eee2de5f007dbe05313
levi7778 <habakukktihamer@gmail.com>
2025-11-02 21:06:53 +0100

    middleware auth.js if rész hozzáadása

---
+++ backend/src/middleware/auth.js
@@ -0,0 +1,8 @@
+import jwt from 'jsonwebtoken';
+
+export const authenticateToken = (req, res, next) => {
+  const authHeader = req.headers['authorization'];
+  const token = authHeader && authHeader.split(' ')[1]; // Bearer TOKEN
+
+
+
