e46039486895eef06bb786b936902e12ccc349a6
levi7778 <habakukktihamer@gmail.com>
2025-11-02 21:08:01 +0100

    middleware auth.js try rész hozzáadása

--- backend/src/middleware/auth.js
+++ backend/src/middleware/auth.js
@@ -4,5 +4,6 @@ export const authenticateToken = (req, res, next) => {
   const authHeader = req.headers['authorization'];
   const token = authHeader && authHeader.split(' ')[1]; // Bearer TOKEN
 
-
-
+ if (!token) {
+    return res.status(401).json({ message: 'Authentication required' });
+  }
